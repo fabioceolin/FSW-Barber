568ca3912e8ce04c53313d264ae11fe97be2ca2a
Fabio Ceolin <fabioceolin98@gmail.com>
2024-01-31 20:57:44 -0300

    feta: ensure unauthenticated user is redirected to login on booking click

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { db } from '@/app/_lib/prisma';
 import BarbershopInfo from './_components/barbershop-info';
 import ServiceItem from './_components/service-item';
+import { getServerSession } from 'next-auth';
+import { authOption } from '@/app/api/auth/[...nextauth]/route';
 
 interface BarberShopDetaisPageProps {
   params: {
@@ -9,6 +11,7 @@ interface BarberShopDetaisPageProps {
 }
 
 const BarberShopDetailsPage = async ({ params }: BarberShopDetaisPageProps) => {
+  const session = await getServerSession(authOption);
   if (!params.id) {
     // TODO: redirecionar para a home page
     return null;
@@ -33,7 +36,11 @@ const BarberShopDetailsPage = async ({ params }: BarberShopDetaisPageProps) => {
 
       <div className='px-5 flex flex-col gap-4 py-6'>
         {barbershop.services.map((service) => (
-          <ServiceItem key={service.id} service={service} />
+          <ServiceItem
+            key={service.id}
+            service={service}
+            isAuthenticated={!!session?.user}
+          />
         ))}
       </div>
     </div>
