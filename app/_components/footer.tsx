c78a2781003d6e3e7b84b08867f8442f6c824a3a
Fabio Ceolin <fabioceolin98@gmail.com>
2024-02-05 15:05:41 -0300

    feat: ensure semantic are use on footer and header

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
-    <div className='w-full bg-secondary py-6 px-5'>
+    <footer className='w-full bg-secondary py-6 px-5'>
       <p className='text-xs text-gray-400 opacity-75'>
         © 2023 Copyright <span className='font-bold'>FSW Barber</span>
       </p>
-    </div>
+    </footer>
   );
 };
 
